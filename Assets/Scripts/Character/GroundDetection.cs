93e26db3170915dc5cfe52e990eb5cc67c2f3027
Guillermo García <55685909+ggarredondo@users.noreply.github.com>
2023-12-18 16:25:40 +0100

    Deleted deprecated script

--- Assets/Scripts/Character/GroundDetection.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public class GroundDetection : MonoBehaviour
-{
-    public System.Action OnLand, OnTakeOff;
-    private void OnTriggerEnter(Collider other) => OnLand?.Invoke();
-    private void OnTriggerExit(Collider other) => OnTakeOff?.Invoke();
-}
