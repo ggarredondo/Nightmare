336ac1e31901249ebc3e72de6de7128c5508f633
Guillermo García <55685909+ggarredondo@users.noreply.github.com>
2023-12-22 19:37:35 +0100

    Added keyboard controls

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,5 +3,10 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     [SerializeField] [Range(0f, 1f)] private float timeScale = 1f;
+    private void Awake()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
     private void OnValidate() => Time.timeScale = timeScale;
 }
